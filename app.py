939f6e2e5e00acec07e1c7e6ae255b618a71cf26
Pratyush ojha <82443757+pratyushojha04@users.noreply.github.com>
2024-11-26 15:48:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -428,4 +428,4 @@ def nearest_gym():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=5002, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=10000, debug=True, allow_unsafe_werkzeug=True)
